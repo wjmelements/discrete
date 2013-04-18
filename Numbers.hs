f5f908beb9bf224840f2aabc0adc727da785ef10
William Morriss <wjmelements@gmail.com>
2013-04-17 22:16:27 -0500

    negative rationals supported

--- Numbers.hs
+++ Numbers.hs
@@ -1,7 +1,7 @@
 module Numbers
 ( naturals
 , integers
-, rationals -- currently only positive
+, rationals
 ) where
 
 naturals :: [Integer]
@@ -11,4 +11,12 @@ integers :: [Integer]
 integers = [if even x then negate $ div x 2 else div (x-1) 2 | x <- naturals]
 
 rationals :: [Double]
-rationals = [(fromIntegral(a-b))/(fromIntegral(b+1)) | a <- naturals, b <- [0..a], 1 == gcd (a-b) (b+1)]
+rationals = 0:
+		[
+			let c = div b 2 in 
+				if even b then fromIntegral(a-c)/fromIntegral(c+1) 
+				else negate $ (fromIntegral(a-c)/fromIntegral(c+1))
+			| a <- naturals
+			, b <- [0..(a*2-1)]
+			, let c = div b 2 in 1 == gcd (a-c) (c+1)
+		]
