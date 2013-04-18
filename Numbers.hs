d19cfd13d94307574f4fb53faa7134c1da41b3ef
William Morriss <wjmelements@gmail.com>
2013-04-17 20:59:16 -0500

    rationals and integers

--- Numbers.hs
+++ Numbers.hs
@@ -1,5 +1,14 @@
 module Numbers
 ( naturals
+, integers
+, rationals -- currently only positive
 ) where
+
 naturals :: [Integer]
 naturals = [1..]
+
+integers :: [Integer]
+integers = [if even x then negate $ div x 2 else div (x-1) 2 | x <- naturals]
+
+rationals :: [Double]
+rationals = [(fromIntegral(a-b))/(fromIntegral(b+1)) | a <- naturals, b <- [0..a], 1 == gcd (a-b) (b+1)]
